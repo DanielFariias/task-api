e78b339b74119b904e220f40f056305153cf6b8e
DanielFariias <danielfariias15@gmail.com>
2024-01-18 19:19:19 -0300

    Add input validation for title and description

--- src/controllers/create-task.js
+++ src/controllers/create-task.js
@@ -3,6 +3,12 @@ import { randomUUID } from "node:crypto";
 import { db } from "../db.js";
 
 export function createTask(req, res) {
+  const hasTitle = req.body.title ? true : false;
+  const hasDescription = req.body.description ? true : false;
+
+  if (!hasTitle) return res.writeHead(200).end(JSON.stringify({ error: 'Title is required'}));
+  if (!hasDescription) return res.writeHead(400).end(JSON.stringify({ error: 'Description is required'}));
+
   const { title, description } = req.body;
 
   const task = ({
